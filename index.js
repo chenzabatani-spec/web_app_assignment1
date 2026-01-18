0fa6719e4b6bbdf7caf4980951632fdc4adf9a6d
shiryjona <shiryj@cs.colman.ac.il>
2026-01-18 14:31:56 +0200

    Add comment Files and logic.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config(); 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const commentRouter = require('./routes/comment_routes');
 
 const postRouter = require('./routes/post_routes');
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
 
 mongoose.connect(process.env.DATABASE_URL);
 
